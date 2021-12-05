434b1d8ecd0ead05537b1953a4219b1bd3c6d450
sahil-naik-kilowott <92841842+sahil-naik-kilowott@users.noreply.github.com>
2021-12-05 20:39:10 +0530

    Add project files.

---
+++ ConsoleApp1/EmiCalculation.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class EmiCalculation
+    {
+
+        internal int p { private get; set; }
+        internal double i { private get; set; }
+
+        internal int t { private get; set; }
+
+        internal int n{ private get; set; }
+
+        internal EmiResult GetEmiResult()
+        {
+            var emi=new EmiResult();
+            emi.calculate = CalculateEmi();
+            return emi;
+        }
+
+        private double CalculateEmi()
+        {
+            double result = 0;
+
+            result =  (1 + (i / n));
+
+            result = p*Math.Pow(result,(t*n));
+
+            result = result / (t * n);
+
+            result = Math.Round(result, 2);
+
+            
+
+            return result;
+
+        }
+
+
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,44 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+using System;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+
+        public static void Main(string[] args)
+        {
+
+
+            Console.Write("Enter the Price of the Vehicle: ");
+            int p= Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter Rate of interest: ");
+            double rate= Convert.ToInt32(Console.ReadLine());
+            double i =rate / 100;
+            Console.Write("Enter Loan Duration: ");
+            int t= Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter Monthly Compounding: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            
+
+            var spObj = new EmiCalculation();
+
+            spObj.p = p;
+            spObj.i = i;
+            spObj.t = t;
+            spObj.n = n;
+
+            var res=spObj.GetEmiResult();
+
+            Console.WriteLine($"Monthly Emi: {res.calculate} INR");
+
+
+
+
+        }
+
+    }
+}
