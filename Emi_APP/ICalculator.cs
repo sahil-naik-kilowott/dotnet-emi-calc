8bd828b1072ece2213e8863ac93d644c6125e5b8
sahil-naik-kilowott <92841842+sahil-naik-kilowott@users.noreply.github.com>
2021-12-07 00:03:34 +0530

    Delete Emi_APP directory

--- Emi_APP/ICalculator.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace EmiApp
-{
-    public interface ICalculator
-    {
-        public Response CalculateMonthlyEmi(Request request);
-        public Response CalculateDailyEmi(Request request);
-        public Response CalculateContinousEmi(Request request);
-    }
-}
