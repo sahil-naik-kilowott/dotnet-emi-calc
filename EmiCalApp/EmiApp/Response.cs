5cf0995a049d1b52d7ce5f93619226eadf35dc87
sahil-naik-kilowott <92841842+sahil-naik-kilowott@users.noreply.github.com>
2021-12-06 23:56:51 +0530

    Delete EmiCalApp directory

--- EmiCalApp/EmiApp/Response.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace EmiApp
-{
-    public class Response
-    {
-        public decimal EmiPayment { get; set; }
-        public string EmiPaymentType { get; set; } = string.Empty; // for example "monthly", "daily", "continuous"
-        public string ErrorMessage { get; set; } = string.Empty;
-    }
-}
