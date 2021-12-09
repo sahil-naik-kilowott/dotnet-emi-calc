127ab1eaa04f81367c830ad9d6088d7010adeb73
sahil-naik-kilowott <92841842+sahil-naik-kilowott@users.noreply.github.com>
2021-12-10 00:15:05 +0530

    Add files via upload

---
+++ EmiApp/Controllers/EmiController.cs
@@ -0,0 +1,57 @@
+﻿
+
+using Microsoft.AspNetCore.Mvc;
+using EmiCalc.Models;
+
+
+
+
+namespace EmiCalc.Controllers
+
+{
+    public class EmiController: Controller
+    {
+        public IActionResult Index()
+        {
+
+            return View();
+        
+        
+        
+        }
+
+       
+
+        public IActionResult Display(double principal, double rate, double duration)
+        {
+
+            Request request = new Request();
+            request.Principal = principal;
+            
+            request.InterestRateInPercentage = rate;
+            request.LoanDurationInYearCount = duration;
+
+            //Calculations
+            double MonthlyEmi = (double)request.Principal * System.Math.Pow(1 + (request.InterestRateInPercentage / 100) / 12, request.LoanDurationInYearCount * 12);
+            
+            double ContinousEmi = (double)request.Principal * System.Math.Pow(2.7183, (request.InterestRateInPercentage / 100 * request.LoanDurationInYearCount));
+            double DailyEmi = ((double)request.Principal * System.Math.Pow(1 + ((request.InterestRateInPercentage / 100) / 365), 365 * request.LoanDurationInYearCount)) - (double)request.Principal;
+           
+            //Rounding the double
+            MonthlyEmi=Math.Round(MonthlyEmi, 2);
+            
+            DailyEmi = Math.Round(DailyEmi, 2);
+            ContinousEmi = Math.Round(ContinousEmi, 2);
+
+            request.DailyEmi = DailyEmi;
+            request.MonthlyEmi = MonthlyEmi;
+            request.ContinousEmi = ContinousEmi;
+
+            return View(request);
+
+
+        }
+
+
+    }
+}
---
+++ EmiApp/Models/Request.cs
@@ -0,0 +1,13 @@
+﻿namespace EmiCalc.Models
+{
+    public class Request
+    {
+
+        public double Principal { internal get; set; }
+        public double InterestRateInPercentage { internal get; set; }
+        public double LoanDurationInYearCount { internal get; set; }
+        public double MonthlyEmi { internal get; set; }
+        public double DailyEmi { internal get; set; }
+        public double ContinousEmi { internal get; set; }
+    }
+}
